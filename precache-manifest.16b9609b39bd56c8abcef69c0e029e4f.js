1f4edf48a1e77ad799b9e5c30154108abca08554
alioguzhan <aoguzhanyildiz@gmail.com>
2019-11-17 01:42:03 +0300

    Updates

--- precache-manifest.16b9609b39bd56c8abcef69c0e029e4f.js
+++ precache-manifest.16b9609b39bd56c8abcef69c0e029e4f.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9babcdf71f4f5bebe73ed198efdcb40b",
+    "revision": "846c376c40456113f40d700b31356662",
     "url": "/react-editext/index.html"
   },
   {
-    "revision": "36e781ca249b316c4b6a",
+    "revision": "96a2f3c2c1d461a13cd2",
     "url": "/react-editext/static/css/main.c61aad12.chunk.css"
   },
   {
-    "revision": "ac0f5ba998e2b0d32a1c",
-    "url": "/react-editext/static/js/2.93d80151.chunk.js"
+    "revision": "f780229aeae924f48565",
+    "url": "/react-editext/static/js/2.d86384f6.chunk.js"
   },
   {
-    "revision": "36e781ca249b316c4b6a",
-    "url": "/react-editext/static/js/main.0cb0bc98.chunk.js"
+    "revision": "96a2f3c2c1d461a13cd2",
+    "url": "/react-editext/static/js/main.be941b93.chunk.js"
   },
   {
     "revision": "4c0d8fdea6ccc819e0ec",
